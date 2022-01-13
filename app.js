8c8c7eb2cf17413fbbdc5b162faf805f3f06ead5
albert199577 <mso729049@gamil.com>
2022-01-13 16:31:08 +0800

    nav bar a underline

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 let nav = document.querySelector("nav");
 let navAnchor = document.querySelectorAll("nav ul li a");
 let logo = document.querySelector(".logo");
+const home = document.querySelector("#home");
+const about = document.querySelector("#about");
+const portfolio = document.querySelector("#portfolio");
+const contact = document.querySelector("#contact");
+let navTag = document.querySelectorAll("nav ul li a[href]");
 
+console.log(navTag[0]);
+//nav bar color switch
 window.addEventListener("scroll", () => {
     if (window.pageYOffset != 0) {
         nav.style.backgroundColor = "rgba(0, 0, 0, .3)";
@@ -18,6 +25,54 @@ window.addEventListener("scroll", () => {
         });
     }
 })
+
+//nav bar a tag text underline
+window.addEventListener("scroll", () => {
+    let windowY = window.pageYOffset;
+    console.log(home.offsetTop);
+    console.log(about.offsetTop);
+    console.log(window.pageYOffset);
+    if (windowY >= 0 || windowPage < about.offsetTop) {
+        clear_underline();
+        navTag[0].classList.add("nav-scroll-underline");
+    } else if (windowPage >= about.offsetTop || windowPage < portfolio.offsetTop) {
+        clear_underline();
+        navTag[1].classList.add("nav-scroll-underline");
+    } else if (windowPage >= portfolio.offsetTop || windowPage < contact.offsetTop) {
+        clear_underline();
+        navTag[2].classList.add("nav-scroll-underline");
+    }
+    //   switch (windowPage) {
+    //     case 0:
+    //       clear_underline();
+    //       navTag[0].classList.add("nav-scroll-underline");
+    //       console.log("aaa");
+    //       break;
+    //     case about.offsetTop:
+    //       clear_underline();
+    //       navTag[1].classList.add("nav-scroll-underline");
+    //       console.log("aaa");
+    //       break;
+    //     case portfolio.offsetTop:
+    //       clear_underline();
+    //       navTag[2].classList.add("nav-scroll-underline");
+    //       console.log("aaa");
+    //       break;
+    //     case contact.offsetTop:
+    //       clear_underline();
+    //       navTag[3].classList.add("nav-scroll-underline");
+    //       console.log("aaa");
+    //       break;
+    //   }
+});
+
+function clear_underline() {
+    navTag.forEach(e => {
+        e.classList.remove("nav-scroll-underline");
+    })
+} 
+
+
 //option list switch
 let optionBtn = document.querySelectorAll("ul.protfolio-list-menu li");
 
