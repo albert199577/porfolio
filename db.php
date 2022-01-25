37da1a764be6a4a081c5adf0ffef3aed850d3555
albert199577 <mso729049@gamil.com>
2022-01-25 10:18:52 +0800

    db file finish

--- db.php
+++ db.php
@@ -69,9 +69,59 @@ class DB {
         }
         return $this->pdo->query($sql)->fetchColumn();
     }
-    
+    public function save($array) {
+        if (isset($array['id'])) {
+            //update 
+            foreach ($array as $key => $value) {
+                $tmp[] = "`$key` = '$value'";
+                $set = implode(" ,", $tmp);
+            }
+            $sql = "UPDATE $this -> table SET " . implode(" ,", $tmp) . "WHERE `id` = '{$array['id']}'";
+        } else {
+            //insert
+            $a = array_keys($array);
+            $b = array_values($array);
+            foreach ($a as $key => $value) {
+                $k[] = "`$value`";
+            }
+            foreach ($b as $key => $value) {
+                $v[] = "'$value'";
+            }
+            $sql = "INSERT INTO $this -> table (" . implode(" ,", $k) .") VALUES(" . implode(" ,", $v) . ")";
+            echo $sql;
+        }
+        return $this->pdo->exec($sql);
+    }
 
+    public function del($id) {
+        $sql = "DELETE FROM $this -> table WHERE ";
+        if (is_array($id)) {
+            foreach ($id as $key => $value) {
+                $tmp[] = "`$key` = '$value'";
+                $sql .= implode(" AND ", $tmp);
+            }
+        } else {
+            $sql .= "`id` = '$id'"; 
+        }
+        return $this->pdo->exec($sql);
+    }
+
+    public function q($sql) {
+        return $this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+    }
 }
+
+function dd($arr) {
+    echo "<pre>";
+    print_r($arr);
+    echo "</pre>";
+}
+
+function to($a) {
+    header("location:" . $a);
+}
+
+
 
 
 ?>
